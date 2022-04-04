1a355de2caa03c6c8d501d456920da71955eb422
Victor Martinez Jr <victormtzcodes@gmail.com>
2022-04-04 16:07:16 -0400

    added responsive styling to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,24 @@ const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
 
   return (
-    <div className="navbar h-[6vh] flex justify-between items-center w-full px-3 relative font-Roboto text-white bg-lightBlack">
-      <h1 className="text-xl italic tracking-wide font-bold">ZA Sport</h1>
+    <div className="navbar h-[6vh] flex justify-between items-center w-full px-3 relative font-Roboto text-white bg-lightBlack lg:px-0 lg:h-[10vh]">
+      <h1 className="text-xl italic tracking-wide font-bold lg:ml-10 lg:text-2xl 2xl:text-3xl">
+        ZA Sport
+      </h1>
       <HiMenuAlt3
         onClick={() => setShowNav(!showNav)}
-        className="cursor-pointer text-2xl"
+        className="cursor-pointer text-2xl lg:hidden"
       />
       <ul
         className={`absolute w-[100%] right-0 top-[100%] flex flex-col items-end pr-3 transform ${
           showNav ? "translate-x-[0]" : " translate-x-[100%]"
-        } transition-transform bg-lightBlack text-white`}
+        } transition-transform bg-lightBlack text-white lg:static lg:flex-row lg:justify-end lg:items-center lg:translate-x-0 lg:w-auto`}
       >
-        <li className="my-4">About Us</li>
-        <li className="my-4">Membership</li>
-        <li className="my-4">Services</li>
-        <li className="my-4">Gallery</li>
-        <li className="my-4">Reviews</li>
+        <li className="my-4 lg:my-0 mr-10 lg:p-4">About Us</li>
+        <li className="my-4 lg:my-0 mr-10 lg:p-4">Membership</li>
+        <li className="my-4 lg:my-0 mr-10 lg:p-4">Services</li>
+        <li className="my-4 lg:my-0 mr-10 lg:p-4">Gallery</li>
+        <li className="my-4 lg:my-0 mr-10 lg:p-4">Reviews</li>
       </ul>
     </div>
   );
